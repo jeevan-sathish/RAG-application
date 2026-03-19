2bc3e9753b82303e8103fae58447419966779e0e
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-19 11:41:11 +0530

    working with msql and python

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,17 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-print(os.getenv("TEXT"))
+from db import get_connection
+
+conn =get_connection()
+cursor =conn.cursor()
+
+
+print("connected succesfully")
+
+query="INSERT INTO userAuth(email,password) values(%s,%s)"
+val=("jeev@gmail.com","Jeev1234")
+cursor.execute(query,val)
+conn.commit()
+
+print("insertted")
+
+cursor.close()
+conn.close()
