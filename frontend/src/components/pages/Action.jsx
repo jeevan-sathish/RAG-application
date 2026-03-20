929bb6222bf6efde9c9299b857acd336e624c700
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-20 21:24:42 +0530

    action page handled

--- frontend/src/components/pages/Action.jsx
+++ frontend/src/components/pages/Action.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Action = () => {
   return (
-    <div className='w-full h-screen flex flex-col justify-center items-center bg-green-100'>Action</div>
+    <div className='w-full h-screen flex flex-col justify-center items-center bg-green-100 '>
+    Action PAGE RENDER
+    </div>
   )
 }
 
