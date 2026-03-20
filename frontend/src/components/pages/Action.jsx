b1d352d024115c5e680f115634117c2cfe2c5f74
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-20 21:25:21 +0530

    action page

--- frontend/src/components/pages/Action.jsx
+++ frontend/src/components/pages/Action.jsx
@@ -4,6 +4,7 @@ const Action = () => {
   return (
     <div className='w-full h-screen flex flex-col justify-center items-center bg-green-100 '>
     Action PAGE RENDER
+    <h1>hello</h1>
     </div>
   )
 }
