dbb77605bb0e9ec92e4c0b43940f5198d5a55b9b
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-21 21:19:20 +0530

    login succes page handled

--- frontend/src/components/auth/SignIN.jsx
+++ frontend/src/components/auth/SignIN.jsx
@@ -1,123 +1,116 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { FaRocket } from "react-icons/fa";
-import { Link, useNavigate } from 'react-router-dom';
-
-
+import { Link, useNavigate } from "react-router-dom";
+import { FaCheckCircle } from "react-icons/fa";
 
 const SignIN = () => {
-    const navigate =useNavigate()
+  const navigate = useNavigate();
 
-    const [data,setData]=useState({
-        email:"",
-        password:""
-    })
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [res,setRes]=useState("")
-    const [islogged,setIsLogged]= useState(false)
+  const [res, setRes] = useState("");
+  const [islogged, setIsLogged] = useState(false);
 
-    function handleData(e){
-        const {name,value }=e.target;
-        setData((prev)=>({
-            ...prev,
-            [name]:value
-        }))
+  function handleData(e) {
+    const { name, value } = e.target;
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
 
-    }
+  async function handleSubmit() {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-     async function handleSubmit(){
-        try{
-            const response = await fetch("http://127.0.0.1:5000/signin",{
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
-                },
-                body:JSON.stringify(data)
-            })
-
-            const result = await response.json()
-            setRes(result.message)
-            if( result.message =="login succesfull"){
-                setIsLogged(true)
-            }
-            else if(result.message =="login failed"){
-                navigate('/SignUP')
-            }
-            else{
-                setIsLogged(false)
-            }
-            setTimeout(()=>{
-                if(result.message=="login succesfull"){
-                        setIsLogged(false)
-                        navigate("/Action")
-                }
-               
-            },3000)
-          
-
+      const result = await response.json();
+      setRes(result.message);
+      if (result.message == "login succesfull") {
+        setIsLogged(true);
+      } else if (result.message == "login failed") {
+        navigate("/SignUP");
+      } else {
+        setIsLogged(false);
+      }
+      setTimeout(() => {
+        if (result.message == "login succesfull") {
+          setIsLogged(false);
+          navigate("/Action");
         }
-        catch(err){
-            console.log("error:",err)
-        }
-
-
-       
-
+      }, 1000);
+    } catch (err) {
+      console.log("error:", err);
     }
+  }
   return (
-   <>
-    {
-        !islogged?(
-             <div className='w-[400px] h-[300px] rounded-2xl p-3 flex flex-col
-     bg-black shadow-2xl shadow-amber-200 justify-evenly items-center'>
-   <div className='w-full animate-pulse flex flex-row justify-center items-center gap-2'>
-     <p className='text-center text-[30px] font-extrabold text-blue-700'>
-     SignIn Here!
-     </p>
-     <FaRocket className='text-blue-500 text-[30px]' />
+    <>
+      {!islogged ? (
+        <div
+          className="w-[400px] h-[300px] rounded-2xl p-3 flex flex-col
+     bg-black shadow-2xl shadow-amber-200 justify-evenly items-center"
+        >
+          <div className="w-full animate-pulse flex flex-row justify-center items-center gap-2">
+            <p className="text-center text-[30px] font-extrabold text-blue-700">
+              SignIn Here!
+            </p>
+            <FaRocket className="text-blue-500 text-[30px]" />
+          </div>
 
-   </div>
+          <input
+            type="email"
+            name="email"
+            value={data.email}
+            placeholder="Enter your email"
+            onChange={handleData}
+            required
+            className="p-3 w-[80%] h-[45px] bg-gray-600 border border-black rounded-2xl "
+          />
 
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            placeholder="Enter your password"
+            onChange={handleData}
+            required
+            className="p-3 w-[80%] h-[45px] bg-gray-400 border border-black rounded-2xl "
+          />
 
-    <input 
-     type="email"
-     name='email'
-     value={data.email}
-     placeholder='Enter your email' 
-     onChange={handleData} required
-     className='p-3 w-[80%] h-[45px] bg-gray-600 border border-black rounded-2xl '   
-     />
+          <button
+            className="w-[80%] h-[35px] border border-amber-300 bg-green-500 text-black rounded-2xl"
+            onClick={handleSubmit}
+          >
+            submit
+          </button>
 
-     <input 
-     type="password"
-     name='password'
-     value={data.password}
-     placeholder='Enter your password' 
-     onChange={handleData} required
-     className='p-3 w-[80%] h-[45px] bg-gray-400 border border-black rounded-2xl '   
-     />
+          <p className="text-blue-300 text-[15px]">
+            create or Register now{" "}
+            <Link
+              to="/SignUP"
+              className="text-red-300 underline hover:text-green-400"
+            >
+              Register!
+            </Link>
+          </p>
 
-     <button 
-      className='w-[80%] h-[35px] border border-amber-300 bg-green-500 text-black rounded-2xl'
-      onClick={handleSubmit}
-       >
-       submit
-       </button>
+          <h1 className="text-white">{res}</h1>
+        </div>
+      ) : (
+        <div className="text-white ">
+          <FaCheckCircle className="w-[100px] h-[100px]  bg-green text-white animate-bounce" />
+        </div>
+      )}
+    </>
+  );
+};
 
-       <p className='text-blue-300 text-[15px]'>create or Register now <Link to='/SignUP' className='text-red-300 underline hover:text-green-400'>Register!</Link></p>
-
-       <h1 className='text-white'>{res}</h1>
-
-       
-    
-    </div>
-        ):(
-            <div className='text-white'>
-                 success✅
-            </div>
-        )
-    }
-   </>
-  )
-}
-
-export default SignIN
+export default SignIN;
--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaInfoCircle } from "react-icons/fa";
 import { ThreeDots } from "react-loader-spinner";
+import { LuMoveRight } from "react-icons/lu";
 
 const Home = () => {
-  const [text,setText]=useState("")
-  const [output,setOutput]=useState([])
-  const [chunkSize,setChunkSize]=useState(0)
-  const [vectorLen,setVectorLen]=useState(0)
-  const [eachVectorlen,setEachVectorLen]=useState(0)
-  const navigate =useNavigate()
+  const [text, setText] = useState("");
+  const [output, setOutput] = useState([]);
+  const [chunkSize, setChunkSize] = useState(0);
+  const [vectorLen, setVectorLen] = useState(0);
+  const [eachVectorlen, setEachVectorLen] = useState(0);
+  const navigate = useNavigate();
 
   const ragExplanation = `
   RAG stands for Retrieval-Augmented Generation.
@@ -34,107 +35,129 @@ It improves reliability without retraining the model.
 Overall, RAG makes AI smarter by using external knowledge......
 `;
 
-function handleText(e){
-  setText(e.target.value)
-
-}
-
-async function handleSubmit(){
-  if(text){
-    try{
-    const response = await fetch("http://127.0.0.1:5000/result",{
-      method:"POST",
-      headers:{
-        "Content-Type":"application/json"
-      },
-      body:JSON.stringify({prompt:text})
-    })
-
-    const result = await response.json()
-    setOutput(result.result)
-    setChunkSize(result.chunkSize)
-    setVectorLen(result.vecLen)
-    setEachVectorLen(result.eachVecLen)
-    console.log(result.result)
-    setText(" ")
-
-
+  function handleText(e) {
+    setText(e.target.value);
   }
-  catch(err){
-    console.log("error:",err)
-  }
-  }
-  else{
-    navigate('/')
-  }
-  
-}
 
-function handleClear(){
-  setOutput([])
-}
+  async function handleSubmit() {
+    if (text) {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/result", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: text }),
+        });
+
+        const result = await response.json();
+        setOutput(result.result);
+        setChunkSize(result.chunkSize);
+        setVectorLen(result.vecLen);
+        setEachVectorLen(result.eachVecLen);
+        console.log(result.result);
+        setText(" ");
+      } catch (err) {
+        console.log("error:", err);
+      }
+    } else {
+      navigate("/");
+    }
+  }
+
+  function handleClear() {
+    setOutput([]);
+  }
+
+  function handleContinue() {
+    navigate("/SignIN");
+  }
   return (
-    <div className='w-full h-[90vh] bg-gray-900 flex flex-row gap-2  '>
+    <div className="w-full h-[90vh] bg-gray-900 flex flex-row gap-2  ">
+      <div className="contentPage w-[40%] h-[85vh] shadow-2xl shadow-blue-900 rounded-2xl m-[20px] border border-gray-800 bg-black p-[30px]">
+        <div className="w-full h-[70px] flex flex-row justify-center items-center">
+          <p className="text-[22px] text-amber-300">
+            {" "}
+            <span className="text-[35px] font-extrabold">RAG</span>(Retrieval
+            Augmented Generation)
+          </p>
+        </div>
+        <p
+          className="text-gray-600 text-[14px] hover:animate-pulse"
+          style={{ fontFamily: "Fira Code, monospace" }}
+        >
+          {ragExplanation}
+        </p>
+      </div>
 
-       <div className='contentPage w-[40%] h-[85vh] shadow-2xl shadow-blue-900 rounded-2xl m-[20px] border border-gray-800 bg-black p-[30px]'>
-          <div className='w-full h-[70px] flex flex-row justify-center items-center'>
-             <p className='text-[22px] text-amber-300'> <span className='text-[35px] font-extrabold'>RAG</span>(Retrieval Augmented Generation)</p>
+      <div className="w-[55%] h-[85vh] bg-tranparent m-[20px] flex flex-col gap-[30px] justify-center items-center p-[40px]">
+        <p className="text-[50px] text-center text-white leading-13.5">
+          Wellcome,Try The new <span className="text-blue-500">RAG</span> Tech
+        </p>
+        <div className="w-[80%] min-h-[200px] p-[20px] rounded-2xl bg-gray-800 border border-black flex flex-col">
+          <p className="text-amber-600">let's try processing.....</p>
+          <div className="w-full flex flex-row gap-4 mt-[10px]">
+            <input
+              type="text"
+              value={text}
+              onChange={handleText}
+              className="w-[300px] h-[30px] pl-[30px] text-green-400  rounded-[5px] border border-black"
+              placeholder="ask about Rag?"
+            />
+            <button
+              onClick={handleSubmit}
+              className="text-red-600 font-extrabold animate-pulse hover:scale-120"
+            >
+              fetch()
+            </button>
+            <button onClick={handleClear} className="text-teal-300">
+              clear()
+            </button>
           </div>
-          <p className='text-gray-600 text-[14px] hover:animate-pulse'   style={{ fontFamily: "Fira Code, monospace" }}>{ragExplanation}</p>
-       </div>
 
-       <div className='w-[55%] h-[85vh] bg-tranparent m-[20px] flex flex-col gap-[30px] justify-center items-center p-[40px]'>
-          <p className='text-[50px] text-center text-white leading-13.5'>Wellcome,Try The new <span className='text-blue-500'>RAG</span> Tech</p>
-            <div className='w-[80%] min-h-[200px] p-[20px] rounded-2xl bg-gray-800 border border-black flex flex-col'>
-                <p className='text-amber-600'>let's try processing.....</p>
-                <div className='w-full flex flex-row gap-4 mt-[10px]'>
-                  <input
-                   type="text"
-                   value={text}
-                   onChange={handleText} 
-                   className='w-[300px] h-[30px] pl-[30px] text-green-400  rounded-[5px] border border-black' placeholder='ask about Rag?'/>
-                  <button onClick={handleSubmit} className='text-red-600 font-extrabold animate-pulse hover:scale-120'>fetch()</button>
-                  <button onClick={handleClear} className='text-teal-300'>clear()</button>
-                </div>
-
-                {
-                  output.length>0?(
-                    <div className='w-[80%] h-auto p-[10px]'>
-                    <p className='text-gray-400'>{output[0]}</p>
-                    
-
-                  </div>
-                  ):(
-                    <div className='w-full flex flex-row justify-center items-center'>
-                      <ThreeDots
-                          visible={true}
-                          height="80"
-                          width="80"
-                          color="#4fa94d"
-                          radius="9"
-                          ariaLabel="three-dots-loading"
-                          wrapperStyle={{}}
-                          wrapperClass=""
-                          />
-                        
-                    </div>
-                  )
-                }
-
-                
-
+          {output.length > 0 ? (
+            <div className="w-[80%] h-auto p-[10px]">
+              <p className="text-gray-400">{output[0]}</p>
             </div>
+          ) : (
+            <div className="w-full flex flex-row justify-center items-center">
+              <ThreeDots
+                visible={true}
+                height="80"
+                width="80"
+                color="#4fa94d"
+                radius="9"
+                ariaLabel="three-dots-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+              />
+            </div>
+          )}
+        </div>
 
-            <div className='w-[85%] h-[50px] flex flex-row justify-center  gap-[15px] items-center shrink-0 p-[10px] bg-black rounded-[5px] text-white'>
-                  <FaInfoCircle className='text-[20px] text-amber-700'/>
-                  <h1>chunk_size : <span className='text-red-500'>{chunkSize}</span></h1>
-                    <h1>vector_Len : <span className='text-red-500'>{vectorLen}</span></h1>
-                      <h1>vector_len[0]: <span className='text-red-500'>{eachVectorlen}</span></h1>
-                </div>
+        <div className="w-[85%] h-[50px] flex flex-row justify-center  gap-[15px] items-center shrink-0 p-[10px] bg-black rounded-[5px] text-white">
+          <FaInfoCircle className="text-[20px] text-amber-700" />
+          <h1>
+            chunk_size : <span className="text-red-500">{chunkSize}</span>
+          </h1>
+          <h1>
+            vector_Len : <span className="text-red-500">{vectorLen}</span>
+          </h1>
+          <h1>
+            vector_len[0]: <span className="text-red-500">{eachVectorlen}</span>
+          </h1>
+        </div>
 
-       </div>
+        <button
+          onClick={handleContinue}
+          className="w-[200px] h-[40px] flex flex-row text-center justify-center items-center text-white gap-2.5 rounded-[10px] bg-blue-500 hover:scale-110"
+        >
+          <LuMoveRight />
+          Continue
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
