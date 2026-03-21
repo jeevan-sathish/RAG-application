007ed9ca783be6653919150a7847d03da28f9c16
Jeevan s <sathishjeevan2004@gmail.com>
2026-03-21 20:52:01 +0530

    managed all packages and runned from stract setup

--- backend/db.py
+++ backend/db.py
@@ -8,5 +8,6 @@ def get_connection():
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_DATABASE")
+            database=os.getenv("DB_DATABASE"),
+             port=int(os.getenv("DB_PORT", 3306))
     )
